d1eda504738ef50a53b39330277ca6e803aa492e
Kevson Filipe <kevsonfilipesantos@gmail.com>
2022-04-02 02:49:16 -0300

    [ADD]

--- src/pages/api/email.ts
+++ src/pages/api/email.ts
@@ -29,25 +29,24 @@ export default async function sendEmail(request: NextApiRequest, response: NextA
 
     try {
 
-
-      await transporter.sendMail({
-        from: process.env.EMAIL,
-        to: process.env.EMAIL,
-        replyTo: data.email,
-        subject: "[CONTATO] " + data.name,
-        html: `
-          <h2>Por que você deseja ser um de nossos mentorados?</h2>
-          <h4>${data.name}</h4>
-          <p>E-mail: ${data.email}</p>
-          <p>${data.message}</p>
-        `
-      });
-      response.status(200).json({ message: "E-mail enviado" })
+      // await transporter.sendMail({
+      //   from: process.env.EMAIL,
+      //   to: process.env.EMAIL,
+      //   replyTo: data.email,
+      //   subject: "[CONTATO] " + data.name,
+      //   html: `
+      //     <h2>Por que você deseja ser um de nossos mentorados?</h2>
+      //     <h4>${data.name}</h4>
+      //     <p>E-mail: ${data.email}</p>
+      //     <p>${data.message}</p>
+      //   `
+      // });
+      return response.status(200).json({ message: "E-mail enviado" })
     } catch (error) {
       console.log(error)
-      response.status(500).json({ message: "Houve um erro interno no servidor estamos tentando resolve-lo" })
+      return response.status(500).json({ message: "Houve um erro interno no servidor estamos tentando resolve-lo" })
     }
   } else {
-    response.status(400).json({ message: "Not Found" });
+    return response.status(400).json({ message: "Not Found" });
   }
 }
