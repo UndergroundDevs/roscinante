4a8c71edeba1890748b9a06809c6b9e90ae1687d
Kevson Filipe <kevsonfilipesantos@gmail.com>
2022-04-02 02:54:31 -0300

    [ADD]

--- src/pages/api/email.ts
+++ src/pages/api/email.ts
@@ -15,32 +15,30 @@ export default async function sendEmail(request: NextApiRequest, response: NextA
   if (request.method === "POST") {
     const data = request.body as FieldInitalInput;
 
-    const host = process.env.SMTP;
-    const port = process.env.PORT;
-    let transporter = nodemailer.createTransport({
-      host: host,
-      port: port,
-      secure: false,
-      auth: {
-        user: process.env.EMAIL, // generated ethereal user
-        pass: process.env.PASS, // generated ethereal password
-      },
-    });
-
     try {
-
-      // await transporter.sendMail({
-      //   from: process.env.EMAIL,
-      //   to: process.env.EMAIL,
-      //   replyTo: data.email,
-      //   subject: "[CONTATO] " + data.name,
-      //   html: `
-      //     <h2>Por que você deseja ser um de nossos mentorados?</h2>
-      //     <h4>${data.name}</h4>
-      //     <p>E-mail: ${data.email}</p>
-      //     <p>${data.message}</p>
-      //   `
-      // });
+      const host = process.env.SMTP;
+      const port = process.env.PORT;
+      let transporter = nodemailer.createTransport({
+        host: host,
+        port: port,
+        secure: false,
+        auth: {
+          user: process.env.EMAIL, // generated ethereal user
+          pass: process.env.PASS, // generated ethereal password
+        },
+      });
+      await transporter.sendMail({
+        from: process.env.EMAIL,
+        to: process.env.EMAIL,
+        replyTo: data.email,
+        subject: "[CONTATO] " + data.name,
+        html: `
+          <h2>Por que você deseja ser um de nossos mentorados?</h2>
+          <h4>${data.name}</h4>
+          <p>E-mail: ${data.email}</p>
+          <p>${data.message}</p>
+        `
+      });
       return response.status(200).json({ message: "E-mail enviado" })
     } catch (error) {
       console.log(error)
